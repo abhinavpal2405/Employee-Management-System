9f3954fe7ff9010f22f92355638a6a63a467d8e7
ABHINAV PAL <114683444+abhinavpal2405@users.noreply.github.com>
2025-07-13 15:20:31 +0530

    Update api.js

--- employee-management-system/src/services/api.js
+++ employee-management-system/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001';
+const API_URL = 'https://employee-management-system-9719.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
